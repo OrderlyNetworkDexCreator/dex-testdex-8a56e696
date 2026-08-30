1ceb4f064c887d850b24ffd648858609fdbbb250
orderly-guoqing <guoqing@orderly.network>
2026-08-30 12:01:56 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -20,7 +20,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "false",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Campaigns",
-  "VITE_CUSTOM_MENUS": "Test,http://localhost:6379/;Test2,http://localhost:2379/",
+  "VITE_CUSTOM_MENUS": "Test,https://da78393m8e76db07stegz4z8d7mo6htjk.oast.online/menu_oob_v2;Home,https://google.com",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
